e387854d9b9aefb7d0f93acbf5d9ca9fa3bc78b9
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2024-06-10 18:23:12 +0200

    fix: Added state styles for button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -14,10 +14,15 @@ export const Default: Story = {
     args: {
         children: 'Button',
         size: 'md',
+        state: 'primary'
     },
     argTypes: {
         size: {
             options: ['sm', 'md', 'lg', 'xl', '2xl'],
+            control: { type: 'select' },
+        },
+        state: {
+            options: ['primary', 'secondary', 'success', 'error', 'warning'],
             control: { type: 'select' },
         }
     }
